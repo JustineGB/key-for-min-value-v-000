d499315ed6326508ec762ef5801a87e2e4508093
Justine Bricknell <justinegayton@gmail.com>
2018-06-27 21:50:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,31 +7,16 @@
 # would these methods work?? flatten (change into an array but then would need to change back into a has?)
 #  keep_if
 
-#def key_for_min_value(name_hash)
-#  smallest_value = nil
-#  corresponding_key = nil
-
-#  name_hash.each do |key, value|
-#  if smallest_value == nil || value < smallest_value
-#    smallest_value = value
-#    corresponding_key = key
-#  end
-#end
-#corresponding_key
-#end
-
 def key_for_min_value(name_hash)
   smallest_value = nil
   corresponding_key = nil
 
   name_hash.each do |key, value|
-  if name_hash.empty?
-    return nil
-  elsif
-    value < smallest_value
+  if smallest_value == nil || value < smallest_value
     smallest_value = value
     corresponding_key = key
-    return corresponding_key
   end
 end
+corresponding_key
 end
+
