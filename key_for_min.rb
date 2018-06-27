a5d3b82b2b0b615d70067ce9c1cb8508e296972f
Justine Bricknell <justinegayton@gmail.com>
2018-06-27 21:30:21 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -15,15 +15,15 @@
 #  value.flatten.
 
 #  keep_if { |key, value|
+def key_for_min_value(hash)
+  smallest_key = nil
+  tiniest_value = nil
+  hash.each do |key, value|
+    if tiniest_value == nil || value < tiniest_value
+      tiniest_value = value
+      smallest_key = key
+    end
+  end
+  smallest_key
+end
 
-def key_for_min_value(name_hash)
-  smallest_value = nil
-  name_hash.each do |key, value|
-    if value < smallest_value
-      smallest_value = value
-      return key
-    else
-      return nil
-end
-end
-end
