e5a683cd9d827480098643b4fc994e35f1468a66
Justine Bricknell <justinegayton@gmail.com>
2018-06-27 21:51:37 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,15 +8,14 @@
 #  keep_if
 
 def key_for_min_value(name_hash)
-  smallest_value = nil
-  corresponding_key = nil
+  small_value = nil
+  small_values_key = nil
 
   name_hash.each do |key, value|
-  if smallest_value == nil || value < smallest_value
-    smallest_value = value
-    corresponding_key = key
+  if small_value == nil || value < small_value
+    small_value = value
+    small_values_key = key
   end
 end
-corresponding_key
+    small_values_key
 end
-
